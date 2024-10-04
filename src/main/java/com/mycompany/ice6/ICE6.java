c55a84926c38467e9dad4f13dc4fde65b499b116
RC_Student_lab <RC_Student_lab@10.0.0.110>
2024-10-04 12:43:46 +0000

    IF STATEMENT

---
+++ src/main/java/com/mycompany/ice6/ICE6.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ice6;
+import java.util.Scanner;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class ICE6 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String sentence = scanner.nextLine();
+
+        int vowelCount = countVowels(sentence);
+
+        System.out.println("Number of vowels: " + vowelCount);
+
+        scanner.close();
+    }
+
+    public static int countVowels(String sentence) {
+        int vowelCount = 0;
+        sentence = sentence.toLowerCase(); // Convert to lowercase
+
+        for (int i = 0; i < sentence.length(); i++) {
+            char currentChar = sentence.charAt(i);
+
+            if (currentChar == 'a' || currentChar == 'e' || currentChar == 'i' || currentChar == 'o' || currentChar == 'u') {
+                vowelCount++;
+            }
+        }
+
+        return vowelCount;
+    
+    }
+}
